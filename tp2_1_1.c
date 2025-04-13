c80755b97b592bf4d255ee1c0b474195761e75a1
“Andrea <andreaveronicam96@gmail.com>
2025-04-13 03:28:19 -0300

    corrigiendo errores

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,23 +1,19 @@
-// codigo a completar
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <time.h> 
+
 #define N 20
 
-int main (){
+int main() {
+    int i;
+    double vt[N];
 
-int i;
-double vt[N];
-double *p;
+    srand(time(NULL)); 
 
-srand(time(NULL)); 
-
-p=vt;
-
-    for(i = 0;i<N; i++){
-    *(p+i)=1+rand()%100;
-    printf("%2.f ", *(p+i));
+    for(i = 0; i < N; i++) {
+        vt[i] = 1 + rand() % 100;
+        printf("%2.f ", vt[i]);
     }
 
+    return 0;
 }
